a98475862e60b46da92cdb7cf064d27cb99e4376
Victor Liew <false>
2024-01-16 17:33:28 +0000

    latest update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,17 +6,17 @@ const Contact = () => {
     <section id="contact">
       <h1>GET IN TOUCH</h1>
       <ul>
-        <li>
+        <li className="contact-method">
           <img src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-email-interface-kiranshastry-gradient-kiranshastry.png"/>
-          <a href="mailto:liewvyl@gmail.com">liewvyl@gmail.com</a>
+          <a href="mailto:liewvyl@gmail.com"> liewvyl@gmail.com</a>
         </li>
-        <li>
+        <li className="contact-method">
           <img src="https://img.icons8.com/nolan/256/linkedin.png"/>
-          <a href="https://www.linkedin.com/in/liewvictor/" rel="noopener noreferrer" target="_blank">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/liewvictor/" rel="noopener noreferrer" target="_blank"> LinkedIn</a>
         </li>
-        <li>
+        <li className="contact-method">
           <img src="https://img.icons8.com/nolan/256/github.png"/>
-          <a href="https://github.com/luckymouse11" rel="noopener noreferrer" target="_blank">GitHub</a>
+          <a href="https://github.com/luckymouse11" rel="noopener noreferrer" target="_blank"> GitHub</a>
         </li>
       </ul>
     </section>
